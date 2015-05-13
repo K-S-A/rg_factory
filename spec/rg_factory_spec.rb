e7908e5dd926ed39e89c56a2f2f871c554d72efc
K-S-A <atillas@ukr.net>
2015-05-13 14:57:53 +0300

    Add *~ filter to .gitignore, created rg_factory_spec.rb.

---
+++ spec/rg_factory_spec.rb
@@ -0,0 +1,76 @@
+require 'spec_helper'
+
+describe RgFactory::Factory do
+  context '.new' do
+  
+  end
+  
+  context '#==' do
+  
+  end
+
+  context '#[]' do
+  
+  end
+  
+  context '#[]=' do
+  
+  end
+  
+  context '#each' do
+  
+  end
+  
+  context '#each_pair' do
+  
+  end
+  
+  context '#eql?' do
+  
+  end
+  
+  context '#hash' do
+  
+  end
+  
+  context '#inspect' do
+  
+  end
+  
+  context '#length' do
+  
+  end
+  
+  context '#members' do
+  
+  end
+  
+  context '#select' do
+  
+  end
+  
+  context '#size' do
+  
+  end
+  
+  context '#to_a' do
+  
+  end
+ 
+  context '#to_h' do
+  
+  end
+  
+  context '#to_s' do
+  
+  end
+  
+  context '#values' do
+  
+  end
+  
+  context '#values_at ' do
+  
+  end
+ 
+end
